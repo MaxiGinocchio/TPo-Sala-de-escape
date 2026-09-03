c2dd8f1045d58325e203888ea66d1e1edffe74fc
nachitosaponaro-wq <nachitosaponaro@gmail.com>
2026-09-03 10:35:31 -0300

    Implement main menu with options for user actions

---
+++ menu.py
@@ -0,0 +1,26 @@
+import instrucciones
+while True:
+    print(" elija alguna de estas opciones : 0-Instrucciones / 1-Jugar / 2-cambiar_contraseña / 3-cerrar_sesion")
+    opcion = int(input("ingrese la opcion elejida :"))
+    
+    if opcion == 0 :
+        instrucciones.mostrar()
+        ###funcion instrucciones
+    
+    elif opcion == 1 :
+        print("elegiste la opcion de Jugar")
+        ###funcion jugar
+    elif opcion == 2 :
+        print("elegiste la opcion de Cambiar_contraseña")
+        ###funcion cambiar-contrasenas
+    elif opcion == 3 :
+        print("saliendo...")
+        ###funcion saliendo
+        break
+        
+    else :
+        print("opcion invalida")
+        ###ingrese otra opcion, no se encunetra dentro de las disponibles
+        
+        
+        
