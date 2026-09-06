eec992aa5490f7ae3b33e17da3aba4bd3c4aa5e7
Maximo Ginocchio <maximogmilillo2906@gmail.com>
2026-09-06 16:20:09 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,15 +1,19 @@
-### Garcia Tomas - Saponaro Ignacio
-
 import instrucciones, seguridad, sala1, sala2
 
 
 def main():
+
+    usuario_inicial = "jugador1"
+    contraseña_encriptada = "W6FGncdu?" #Contraseña encriptada con un corrimiento de 2 caracteres (U4DElabs=)
+
     print("\n----------------------------------------")
     print("Bienvenido al escape de prisión\n")
+
+    seguridad.inicio_sesion(usuario_inicial, contraseña_encriptada)
     
     while True:
-        print("Opciones: 0- Instrucciones / 1- Jugar / 2- Cambiar contraseña / 3- Cerrar sesión")
-        opcion = int(input("Ingrese la opcion elejida: "))
+        print("Opciones: \n0- Instrucciones \n1- Jugar \n2- Cambiar contraseña \n3- Cerrar sesión")
+        opcion = int(input("\nIngrese la opcion elejida: "))
         
         if opcion == 0 :
             instrucciones.mostrar()
@@ -17,10 +21,16 @@ def main():
         
         elif opcion == 1 :
             print("Elegiste la opcion de Jugar")
-            ###función jugar
+            resultado = sala1.sala1()
+            if resultado == True:
+                resultado = sala2.sala2()
+            else:
+                break
+
         elif opcion == 2 :
-            print("Elegiste la opcion de Cambiar contraseña")
-            ###función cambiar_contraseña
+            print("Elegiste la opcion de Cambiar contraseña\n")
+            contraseña_encriptada = seguridad.cambiar_contraseña(contraseña_encriptada)
+
         elif opcion == 3 :
             print("Saliendo...")
             ###terminar el programa
