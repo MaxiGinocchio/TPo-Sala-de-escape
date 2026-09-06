98c11fa205891a6de1829a9c39adef461b4616af
Maximo Ginocchio <maximogmilillo2906@gmail.com>
2026-09-06 00:04:00 -0300

    Revise menu handling and user option selection
    Refactor main loop to handle user options with functions.

--- menu.py
+++ menu.py
@@ -1,28 +1,36 @@
 ### Garcia Tomas - Saponaro Ignacio
 
-import instrucciones
-while True:
-    print(" elija alguna de estas opciones : 0-Instrucciones / 1-Jugar / 2-cambiar_contraseña / 3-cerrar_sesion")
-    opcion = int(input("ingrese la opcion elejida :"))
+import instrucciones, seguridad, sala1, sala2
+
+
+def main():
+    print("\n----------------------------------------")
+    print("Bienvenido al escape de prisión\n")
     
-    if opcion == 0 :
-        instrucciones.mostrar()
-        ###funcion instrucciones
-    
-    elif opcion == 1 :
-        print("elegiste la opcion de Jugar")
-        ###funcion jugar
-    elif opcion == 2 :
-        print("elegiste la opcion de Cambiar_contraseña")
-        ###funcion cambiar-contrasenas
-    elif opcion == 3 :
-        print("saliendo...")
-        ###funcion saliendo
-        break
+    while True:
+        print("Opciones: 0- Instrucciones / 1- Jugar / 2- Cambiar contraseña / 3- Cerrar sesión")
+        opcion = int(input("Ingrese la opcion elejida: "))
         
-    else :
-        print("opcion invalida")
-        ###ingrese otra opcion, no se encunetra dentro de las disponibles
+        if opcion == 0 :
+            instrucciones.mostrar()
+            ###función instrucciones
+        
+        elif opcion == 1 :
+            print("Elegiste la opcion de Jugar")
+            ###función jugar
+        elif opcion == 2 :
+            print("Elegiste la opcion de Cambiar contraseña")
+            ###función cambiar_contraseña
+        elif opcion == 3 :
+            print("Saliendo...")
+            ###terminar el programa
+            break
+            
+        else :
+            print("Opción invalida")
+            ###ingrese otra opción, no se encunetra dentro de las disponibles
         
         
         
+if __name__ == "__main__":
+    main()
